050617f380e155d451777772726bb7a44df0f7a7
Ahmed Musawir <ahmed.musawir@gmail.com>
2023-02-28 11:52:17 +0800

    28Feb2023 - added plugin updater option

--- lee-goff-similarweb-react-plugin.php
+++ lee-goff-similarweb-react-plugin.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: Lee Goff Lead Calculator Plugin
+Plugin Name: Traffic2Lead Calculator Plugin
 Plugin URI: https://cyberizegroup.com/
-Description: This can be used as plugin dev starter kit
-Version: 1.0
-Author: The Moose
+Description: This plugin will help you plan how to convert your website traffic into profitable leads.
+Version: 2.0
+Author: Cyberize Group Inc.
 Author URI: https://linkedin.com/ahmedmusawir
 License: GPLv2 or later
 Text Domain: cyberizeframework
@@ -14,6 +14,22 @@ Text Domain: cyberizeframework
 if (!defined('WPINC')) {
  die;
 }
+
+// PLUGIN UPDATE CHECKER
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+ 'https://github.com/ahmedmusawir/lee-goff-similarweb-react-plugin/',
+ __FILE__,
+ 'lee-goff-similarweb-react-plugin'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
 
 // THE FOLLOWING VARIABLE NEEDS TO BE CHANGED TO SOMETHIGN UNIQUE FOR EACH PLUGIN
 define('LEE_GOFF_REACT_PLUGIN_URL', plugin_dir_url(__FILE__));
